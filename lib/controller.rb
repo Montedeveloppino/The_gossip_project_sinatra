9a304d4009162c6d4241e10a4f11ddca9a17fd6e
Youssef <annabiyoussef1986@gmail.com>
2021-02-01 13:41:54 +0100

    final 2.6

--- lib/controller.rb
+++ lib/controller.rb
@@ -1,15 +1,21 @@
+require 'gossip'
 
 class ApplicationController < Sinatra::Base
+
   get '/' do
-    erb :index
+  erb :index, locals: {gossips: Gossip.all}
   end
+
   get '/gossips/new/' do
     erb :new_gossip
   end
+
   post '/gossips/new/' do
-    puts "Ce programme ne fait rien pour le moment, on va donc afficher un message dans le terminal"
+    Gossip.new(params['gossip_author'], params['gossip_content']).save
+    redirect '/'
   end
-  post '/gossips/new/' do
-    Gossip.new.save
-  end
+
+  get '/gossips/:id' do
+  erb :show, locals: {id: params['id'].to_i, gossips: Gossip.find(params['id'].to_i)}
+  end 
 end
--- lib/gossip.rb
+++ lib/gossip.rb
@@ -1,10 +1,32 @@
-require 'bundler'
-Bundler.require
+require 'csv'
 
 class Gossip
+attr_accessor :author, :content
+  def initialize(author, content)
+  @author = author
+  @content = content
+  end
   def save
     CSV.open("./db/gossip.csv", "ab") do |csv|
-      csv << ["Mon super auteur", "Ma super description"]
+      csv << [@author, @content]
     end
+  end
+  def self.all
+    all_gossips = []
+    CSV.read("./db/gossip.csv").each do |csv_line|
+      all_gossips << Gossip.new(csv_line[0], csv_line[1])
+    end
+    return all_gossips
+  end
+
+  def self.find(id)
+      gossips = Array.new
+      CSV.read("./db/gossip.csv").each_with_index do |j, i|
+        if (id == i + 1)
+          gossips << Gossip.new(j[0], j[1])
+          break
+        end
+      end
+      return gossips
   end
 end
